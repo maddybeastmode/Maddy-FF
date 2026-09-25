ec4883f06e28dd9899a4e1a1c175d71c813958e5
maddybeastmode <mudassiralishah4@gmail.com>
2026-09-25 14:05:57 +0500

    Add files via upload

---
+++ src /proto /compiled/__init__.py
@@ -0,0 +1,17 @@
+from . import freefire_pb2
+from . import MajoRLoGinrEq_pb2
+from . import MajoRLoGinrEs_pb2
+from . import dev_generator_pb2
+from . import devxt_count_pb2
+from . import like_pb2
+from . import like_count_pb2
+
+__all__ = [
+    "freefire_pb2",
+    "MajoRLoGinrEq_pb2",
+    "MajoRLoGinrEs_pb2",
+    "dev_generator_pb2",
+    "devxt_count_pb2",
+    "like_pb2",
+    "like_count_pb2",
+]
